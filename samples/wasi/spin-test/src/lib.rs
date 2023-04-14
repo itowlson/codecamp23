d0b581c35aa02fee3a0658695aa1af650c290139
itowlson <ivan.towlson@fermyon.com>
2023-04-14 15:34:50 +1200

    Use an import in the Spin sample

--- samples/wasi/spin-test/src/lib.rs
+++ samples/wasi/spin-test/src/lib.rs
@@ -4,12 +4,23 @@ use spin_sdk::{
     http_component,
 };
 
-/// A simple Spin HTTP component.
+const VC_KEY: &str = "visitor_count";
+
 #[http_component]
-fn handle_spin_test(req: Request) -> Result<Response> {
-    println!("{:?}", req.headers());
+fn handle_spin_test(_req: Request) -> Result<Response> {
+
+    let kv = spin_sdk::key_value::Store::open_default()?;
+    let val = match kv.get(VC_KEY) {
+        Err(spin_sdk::key_value::Error::NoSuchKey) => 0,
+        Err(e) => anyhow::bail!(e),
+        Ok(v) => String::from_utf8(v)?.parse()?,
+    };
+    let count = val + 1;
+    kv.set(VC_KEY, count.to_string().as_bytes())?;
+
+    let message = format!("You are visitor number {count}");
     Ok(http::Response::builder()
         .status(200)
-        .header("foo", "bar")
-        .body(Some("Hello, Fermyon".into()))?)
+        .header("content-type", "text/plain")
+        .body(Some(message.into()))?)
 }
